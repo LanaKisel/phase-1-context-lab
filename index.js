7fdb9e996ff6b01bb6d6294a9f152404f65c58ac
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-10-04 15:36:09 -0700

    timecard and payroll lab

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeArray){
+    const [firstName, familyName, title, payRate] = employeeArray;
+    const employee = {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee
+}
+function createEmployeeRecords(arrayOfEmployeeArrays) {
+return arrayOfEmployeeArrays.map((employeeArray)=> createEmployeeRecord(employeeArray))
+ }
+ function createTimeInEvent(dateStamp){
+    const [date, time] = dateStamp.split(' ');
+    const hour = parseInt(time, 10);
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour,
+        date
+    })
+    return this
+ }
+ function createTimeOutEvent (dateStamp){
+    const [date, time] = dateStamp.split(' ');
+    const hour = parseInt(time,10);
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour,
+        date
+    })
+    return this
+ }
+ function hoursWorkedOnDate(date){
+    const dateIn = this.timeInEvents.find((event) => event.date === date)
+    const dateOut = this.timeOutEvents.find((event) => event.date === date)
+    // if (dateIn && dateOut) {
+    const dateHours = (dateOut.hour-dateIn.hour) / 100;
+    // }
+    return dateHours
+ }
+function wagesEarnedOnDate(date){
+    const wagesEarned = this.payPerHour*hoursWorkedOnDate.call(this, date);
+    return wagesEarned
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +68,10 @@ const allWagesFor = function () {
     return payable
 }
 
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find((employee)=>employee.firstName === firstName);
+    }
+function calculatePayroll(employees){
+return employees.reduce((total,employee) => total + allWagesFor.call(employee), 0)
+}    
